1f7a5cd04f2adfe8839192cf49966a04e15d0653
rian-sousa <riansousadf@gmail.com>
2023-09-18 21:55:55 -0300

    Criacao de pagina de item Atualizacao do model do item (customBancoLib)

---
+++ Customzito/Controllers/ItemController.cs
@@ -0,0 +1,31 @@
+﻿using CustomBancoLib;
+using Customzito.Services.CZDatabase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Customzito.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly CZContext _czContext;
+
+        public ItemController(CZContext context)
+        {
+            _czContext = context;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var obj = new TB_Produto
+            {
+                Titulo = "Broca-Shirt",
+                Descricao = "Blusa do brocasito, versão exclusiva, usada num show em brasília",
+                Preco = 420,
+                qtd = 1,
+                Avaliacao = 3.22M,
+            };
+
+            return View(obj);
+        }
+    }
+}
