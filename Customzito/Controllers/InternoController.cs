ba071f9f9e602672d2919df5a072ed26dcc43d29
rian-sousa <riansousadf@gmail.com>
2023-11-12 19:59:03 -0300

    daily progress

--- Customzito/Controllers/InternoController.cs
+++ Customzito/Controllers/InternoController.cs
@@ -1,4 +1,5 @@
-﻿using Customzito.Services.CZDatabase;
+﻿using CustomBancoLib;
+using Customzito.Services.CZDatabase;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -20,13 +21,30 @@ namespace Customzito.Controllers
         }
 
         [Authorize]
-        public async Task<IActionResult> ControleEstoque()
+        public async Task<IActionResult> ControleEstoque(int? idColecao)
         {
+            Dictionary<string, List<TB_Produto>> DictRoupasEstoques = new Dictionary<string, List<TB_Produto>>();
+
             var Colecoes = await _czContext.TbColecao
                 .ToListAsync();
 
             var Produtos = await _czContext.TbProduto
                 .ToListAsync();
+
+            if(idColecao is not null)
+            {
+                var lstRoupas = await RecuperarRoupasColecao(idColecao);
+
+                DictRoupasEstoques.Add(idColecao.ToString(), lstRoupas);
+
+                Console.WriteLine(DictRoupasEstoques.Values);
+
+                ViewBag.lstProdutosColecao = lstRoupas;
+
+            }
+            
+
+            TempData["RoupasDaColecao"] = DictRoupasEstoques;
 
             ViewBag.lstProdutos = Produtos;
             ViewBag.lstColecoes = Colecoes;
@@ -35,9 +53,30 @@ namespace Customzito.Controllers
         }
 
         [Authorize]
+        public async Task<List<TB_Produto>> RecuperarRoupasColecao(int? idColecao)
+        {
+            if(idColecao is not null)
+            {
+                var ProdColecao = await _czContext.TbProduto
+                .Where(x => x.IdColecao == idColecao)
+                .ToListAsync();
+
+                foreach (var item in ProdColecao)
+                {
+                    Console.WriteLine("="+item.IdProduto+"=");
+                }                
+
+                return ProdColecao;
+            }
+            return default;
+        }
+
+        [Authorize]
         public async Task<IActionResult> Adicionar()
         {
             return default;
         }
+
+
     }
 }
--- Customzito/wwwroot/js/Logado.js
+++ Customzito/wwwroot/js/Logado.js
@@ -4,8 +4,24 @@
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl)
     })
-
-
     
 
 })
+
+
+function RecuperarRoupasColecao(idColecao) {
+
+    $.ajax({
+        url: `ControleEstoque`, // Substitua "SuaAction" e "SeuController" pelos nomes reais
+        type: 'GET', // ou 'GET', dependendo da sua action
+        data: { idColecao },
+        success: function (data) {
+            // Lógica de manipulação de resultado, se necessário
+            /*$('#PartialRoupas').html(data);*/
+        },
+        error: function (error) {
+            // Lógica de tratamento de erro, se necessário
+        }
+    });
+    
+}
