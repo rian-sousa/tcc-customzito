215ca1b1dfe8467ebafc87a6388df307b938575c
rian-sousa <riansousadf@gmail.com>
2023-11-15 23:55:59 -0300

    Tela de controle de estoque finalizado

--- Customzito/Controllers/InternoController.cs
+++ Customzito/Controllers/InternoController.cs
@@ -10,7 +10,7 @@ namespace Customzito.Controllers
     public class InternoController : Controller
     {
         private CZContext _czContext;
-        public InternoController(CZContext contexto) 
+        public InternoController(CZContext contexto)
         {
             _czContext = contexto;
         }
@@ -42,6 +42,8 @@ namespace Customzito.Controllers
 
             ViewBag.TipoDrop = new SelectList(Tipos, "IdTipoVestimenta", "Descricao");
 
+            ViewBag.ColecoesDrop = new SelectList(Colecoes, "IdColecao", "Descricao");
+
             if (idColecao is not null)
             {
                 var lstRoupas = await RecuperarRoupasColecao(idColecao);
@@ -55,7 +57,7 @@ namespace Customzito.Controllers
                 return PartialView("_VisualizerRoupasPartial", lstRoupas);
 
             }
-            
+
 
             TempData["RoupasDaColecao"] = DictRoupasEstoques;
 
@@ -68,7 +70,7 @@ namespace Customzito.Controllers
         [Authorize]
         public async Task<List<TB_Produto>> RecuperarRoupasColecao(int? idColecao)
         {
-            if(idColecao is not null)
+            if (idColecao is not null)
             {
                 var ProdColecao = await _czContext.TbProduto
                 .Where(x => x.IdColecao == idColecao)
@@ -76,8 +78,8 @@ namespace Customzito.Controllers
 
                 foreach (var item in ProdColecao)
                 {
-                    Console.WriteLine("="+item.IdProduto+"=");
-                }                
+                    Console.WriteLine("=" + item.IdProduto + "=");
+                }
 
                 return ProdColecao;
             }
@@ -85,19 +87,168 @@ namespace Customzito.Controllers
         }
 
         [Authorize]
-        public async Task<IActionResult> Adicionar()
+        [HttpPost]
+        public async Task<IActionResult> AdicionarColecao(string DescricaoColecao, bool checkLimitado)
         {
-            return default;
+            if (DescricaoColecao is not null)
+            {
+                TB_Colecao ObjColecao = new TB_Colecao()
+                {
+                    Descricao = DescricaoColecao,
+                    Limitado = checkLimitado
+                };
+
+                await _czContext.TbColecao
+                    .AddAsync(ObjColecao);
+
+                await _czContext.SaveChangesAsync();
+
+                return PartialView("_OperacaoConfirmadaPartial");
+            }
+
+            return PartialView("_OperacaoFalhaPartial");
+
         }
 
         [Authorize]
-        public async Task<IActionResult> RemoverColecao()
+        [HttpPost]
+        public async Task<IActionResult> RemoverColecao(int id)
         {
-            return default;
+            var ObjColecao = await _czContext.TbColecao
+                .Where(x => x.IdColecao == id)
+                .FirstOrDefaultAsync();
+
+            if (ObjColecao is not null)
+            {
+                _czContext.TbColecao
+                .Remove(ObjColecao);
+
+                await _czContext.SaveChangesAsync();
+
+                return PartialView("_OperacaoConfirmadaPartial");
+            }
+
+            return PartialView("_OperacaoFalhaPartial");
         }
 
         [Authorize]
-        public async Task<IActionResult> EditarColecao(int id)
+        [HttpPost]
+        public async Task<IActionResult> EditarColecao(int id, string descricao, bool limitado)
+        {
+            var ObjColecao = await _czContext.TbColecao
+                .FirstOrDefaultAsync(x => x.IdColecao == id);
+
+            ObjColecao.Descricao = descricao;
+            ObjColecao.Limitado = limitado;
+
+            _czContext.Update(ObjColecao);
+
+            await _czContext.SaveChangesAsync();
+
+            return PartialView("_OperacaoConfirmadaPartial");
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> AdicionarRoupa(string DescricaoRoupa, string Titulo, float preco, int IdTipoVestimenta, int IdColecao, decimal? avaliacao, int qtd, string? cor, string IdMaterial, string Marca, IFormFile? imagem)
+        {
+            if (imagem is not null)
+            {
+                var CaminhoArquivo = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads", imagem.FileName);
+
+                using (var stream = new FileStream(CaminhoArquivo, FileMode.Create))
+                {
+                    await imagem.CopyToAsync(stream);
+                }
+
+                TB_Produto ObjColecao = new TB_Produto()
+                {
+                    Descricao = DescricaoRoupa,
+                    Titulo = Titulo,
+                    Preco = preco,
+                    IdTipoVestimenta = IdTipoVestimenta,
+                    IdColecao = IdColecao,
+                    Avaliacao = avaliacao,
+                    qtd = qtd,
+                    Cor = cor,
+                    Material = IdMaterial,
+                    Marca = Marca,
+                    UrlImagem = CaminhoArquivo
+                };
+                await _czContext.TbProduto
+                .AddAsync(ObjColecao);
+
+                await _czContext.SaveChangesAsync();
+
+                return PartialView("_OperacaoConfirmadaPartial");
+            }
+            else
+            {
+                TB_Produto ObjColecao = new TB_Produto()
+                {
+                    Descricao = DescricaoRoupa,
+                    Titulo = Titulo,
+                    Preco = preco,
+                    IdTipoVestimenta = IdTipoVestimenta,
+                    IdColecao = IdColecao,
+                    Avaliacao = avaliacao,
+                    qtd = qtd,
+                    Cor = cor,
+                    Material = IdMaterial,
+                    Marca = Marca
+                };
+
+                await _czContext.TbProduto
+                    .AddAsync(ObjColecao);
+
+                await _czContext.SaveChangesAsync();
+
+                return PartialView("_OperacaoConfirmadaPartial");
+            }
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> RemoverRoupa(int id)
+        {
+            var ObjRoupa = await _czContext.TbProduto
+                .FirstOrDefaultAsync(x => x.IdProduto == id);
+
+            _czContext.TbProduto
+               .Remove(ObjRoupa);
+
+            await _czContext.SaveChangesAsync();
+
+            return PartialView("_OperacaoConfirmadaPartial");
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> EditarRoupa(int idProduto,string DescricaoRoupa, string Titulo, float preco, int IdTipoVestimenta, int IdColecao, decimal? avaliacao, int qtd, string? cor, string IdMaterial, string Marca)
+        {
+            var ObjProduto = await _czContext.TbProduto
+                .FirstOrDefaultAsync(x => x.IdProduto == idProduto);
+
+            ObjProduto.Titulo = Titulo;
+            ObjProduto.Preco = preco;
+            ObjProduto.Descricao = DescricaoRoupa;
+            ObjProduto.IdTipoVestimenta = IdTipoVestimenta;
+            ObjProduto.IdColecao = IdColecao;
+            ObjProduto.Avaliacao = avaliacao;
+            ObjProduto.qtd = qtd;
+            ObjProduto.Marca = Marca;
+            ObjProduto.Cor = cor;
+            ObjProduto.Material = IdMaterial;
+
+            _czContext.Update(ObjProduto);
+
+            await _czContext.SaveChangesAsync();
+
+            return PartialView("_OperacaoConfirmadaPartial");
+        }
+
+        [Authorize]
+        public async Task<IActionResult> RestaurarModalEditarColecao(int id)
         {
             var Colecao = await _czContext.TbColecao
                 .FirstOrDefaultAsync(x => x.IdColecao == id);
@@ -105,6 +256,69 @@ namespace Customzito.Controllers
             return PartialView("_EditarColecaoPartial", Colecao);
         }
 
+
+        [Authorize]
+        public IActionResult RestaurarModalAddColecao()
+        {
+            return PartialView("_ModalAdicionarColecaoPartial");
+        }
+
+        [Authorize]
+        public IActionResult RestaurarModalRemoverColecao(string idColecao)
+        {
+            return PartialView("_ModalRemoverColecaoPartial", idColecao);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> RestaurarModalAddRoupa()
+        {
+            var Colecoes = await _czContext.TbColecao
+                .ToListAsync();
+
+            var Materiais = await _czContext.Material
+                .ToListAsync();
+
+            var Tipos = await _czContext.TdTipoVestimenta
+                .ToListAsync();
+
+            ViewBag.MaterialDrop = new SelectList(Materiais, "IdMaterial", "Descricao");
+
+            ViewBag.TipoDrop = new SelectList(Tipos, "IdTipoVestimenta", "Descricao");
+
+            ViewBag.ColecoesDrop = new SelectList(Colecoes, "IdColecao", "Descricao");
+
+            return PartialView("_ModalAdicionarRoupaPartial");
+        }
+
+        [Authorize]
+        public IActionResult RestaurarModalRemoverRoupa(string idRoupa)
+        {
+            return PartialView("_ModalRemoverRoupaPartial", idRoupa);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> RestaurarModalEditarRoupa(int id)
+        {
+            var Colecoes = await _czContext.TbColecao
+                .ToListAsync();
+
+            var Materiais = await _czContext.Material
+                .ToListAsync();
+
+            var Tipos = await _czContext.TdTipoVestimenta
+                .ToListAsync();
+
+            ViewBag.MaterialDrop = new SelectList(Materiais, "IdMaterial", "Descricao");
+
+            ViewBag.TipoDrop = new SelectList(Tipos, "IdTipoVestimenta", "Descricao");
+
+            ViewBag.ColecoesDrop = new SelectList(Colecoes, "IdColecao", "Descricao");
+
+            var Produto = await _czContext.TbProduto
+                .FirstOrDefaultAsync(x => x.IdProduto == id);
+
+            return PartialView("_EditarRoupaPartial", Produto);
+        }
 
     }
 }
--- Customzito/wwwroot/js/Logado.js
+++ Customzito/wwwroot/js/Logado.js
@@ -26,12 +26,58 @@ function RecuperarRoupasColecao(idColecao) {
     
 }
 
-function AbrirModalEditarColecao(idColecao) {
-    // Lógica para carregar os detalhes da coleção e preencher a partial view
-    // Use AJAX para buscar os dados do servidor e preencher a partial view
-    // Exemplo: $.get('/Colecao/Editar/' + idColecao, function(data) { $('#EditarColecaoPartial').html(data); });
+function MascaraValor(input) {
+    $('#preco').inputmask('R$ 999,99 ', { placeholder: 'R$ ___,__' });
+}
+
+function RemoverColecao(idColecao) {
+
     $.ajax({
-        url: `EditarColecao/` + idColecao,
+        url: `RemoverColecao/` + idColecao,
+        type: 'POST',
+        success: function (data) {
+            $('#RemoverColecaoModalBody').empty()
+            $('#RemoverColecaoModalBody').html(data);
+        }
+    })
+}
+function AdicionarColecao() {
+    let descricao = $('#DescricaoColecao').val();
+    let limitado = $('#checkLimitado');
+
+    let limitadoValue = limitado.is(':checked') ? 'true' : 'false';
+
+    $.ajax({
+        url: `AdicionarColecao/` + '?DescricaoColecao=' + descricao + '&checkLimitado=' + limitadoValue,
+        type: 'POST',
+        success: function (data) {
+            $('#AdicionarColecaoModalBody').empty()
+            $('#AdicionarColecaoModalBody').html(data);
+        }
+    })
+}
+
+function EditarColecao() {
+    let InputId = $('#InputIdColecao').val();
+    let Ndescricao = $('#NovaDescricao').val();
+    let limitado = $('#checkLimitado');
+
+    let limitadoValue = limitado.is(':checked') ? 'true' : 'false';
+
+    $.ajax({
+        url: 'EditarColecao/' + '?id=' + InputId + "&descricao=" + Ndescricao + '&limitado=' + limitadoValue,
+        type: 'POST',
+        success: function (data) {
+            $('#EditarColecaoPartial').empty()
+            $('#EditarColecaoPartial').html(data);
+        }
+    })
+}
+
+function AbrirModalEditarColecao(idColecao) {
+
+    $.ajax({
+        url: `RestaurarModalEditarColecao/` + idColecao,
         type: 'GET',
         success: function (data) {
             $('#EditarColecaoPartial').html(data);
@@ -39,7 +85,116 @@ function AbrirModalEditarColecao(idColecao) {
     })
 }
 
-function MascaraValor(input) {
+function RestaurarModalColecaoAdd(modal) {
+    $.ajax({
+        url: modal,
+        type: 'GET',
+        success: function (data) {
+            $('#AdicionarColecaoModalBody').empty()
+            $('#AdicionarColecaoModalBody').html(data);
+        }
+    })
+}
 
-    $('#preco').inputmask('R$ 999,99 ', { placeholder: 'R$ ___,__' });
+function AbrirModalColecaoRemover(modal,id) {
+    $.ajax({
+        url: modal + `?idColecao=${id}`,
+        type: 'GET',
+        success: function (data) {
+            $('#RemoverColecaoModalBody').empty()
+            $('#RemoverColecaoModalBody').html(data);
+        }
+    })
+}
+
+function AbrirModalRoupaAdd(modal) {
+    $.ajax({
+        url: modal,
+        type: 'GET',
+        success: function (data) {
+            $('#AdicionarColecaoModalBody').empty()
+            $('#AdicionarColecaoModalBody').html(data);
+        }
+    })
+}
+
+function AbrirModalEditarRoupa(idRoupa) {
+
+    $.ajax({
+        url: `RestaurarModalEditarRoupa/` + idRoupa,
+        type: 'GET',
+        success: function (data) {
+            $('#EditarRoupaPartial').html(data);
+        }
+    })
+}
+
+function RestaurarModalRoupaAdd(modal) {
+    $.ajax({
+        url: modal,
+        type: 'GET',
+        success: function (data) {
+            $('#AdicionarRoupaModalBody').empty()
+            $('#AdicionarRoupaModalBody').html(data);
+        }
+    })
+}
+
+function RestaurarModalRoupaRemover(modal) {
+    $.ajax({
+        url: modal,
+        type: 'GET',
+        success: function (data) {
+            $('#AdicionarRoupaModalBody').empty()
+            $('#AdicionarRoupaModalBody').html(data);
+        }
+    })
+}
+
+function AbrirModalRoupaRemover(modal, id) {
+    $.ajax({
+        url: modal + `?idRoupa=${id}`,
+        type: 'GET',
+        success: function (data) {
+            $('#RemoverRoupaModalBody').empty()
+            $('#RemoverRoupaModalBody').html(data);
+        }
+    })
+}
+
+function RemoverRoupa(idRoupa) {
+    $.ajax({
+        url: `RemoverRoupa/` + idRoupa,
+        type: 'POST',
+        success: function (data) {
+            $('#RemoverRoupaModalBody').empty()
+            $('#RemoverRoupaModalBody').html(data);
+        }
+    })
+}
+
+
+function AdicionarRoupa() {
+    let DescricaoRoupa = $('#DescricaoRoupa').val();
+    let Titulo = $('#Titulo').val();
+    let preco = $('#preco').val();
+    let IdTipoVestimenta = $('#IdTipoVestimenta').val();
+    let qtd = $('#qtd').val();
+    let IdMaterial = $('#IdMaterial').val();
+    let IdColecao = $('#IdColecao').val();
+    let Marca = $('#Marca').val();
+    let imagem = $('#imagem').val();
+    
+
+    $.ajax({
+        url: `AdicionarRoupa/` + '?DescricaoRoupa=' + DescricaoRoupa + '&Titulo=' + Titulo
+            + '&preco=' + preco + '&IdTipoVestimenta=' + IdTipoVestimenta + '&qtd=' + qtd
+            + '&IdMaterial=' + IdMaterial + '&IdColecao=' + IdColecao + '&Marca=' + Marca
+            + '&imagem=' + imagem,
+        type: 'POST',
+        success: function (data) {
+            $('#AdicionarRoupaModalBody').empty()
+            $('#AdicionarRoupaModalBody').html(data);
+        }
+    })
 }
